6a0d5a4055a625b01fa8a62ba070c53fc3eaeed5
apetrosyan <apetrosyan@issart.com>
2023-09-18 13:06:22 +0600

    organizations-endpoints: add organizations model and schema file

---
+++ backend/app/app/schemas/organization.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# Shared properties
+class OrganizationBase(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+
+
+# Properties to receive on item creation
+class OrganizationCreate(OrganizationBase):
+    name: str
+    description: str
+
+
+# Properties to receive on item update
+class OrganizationUpdate(OrganizationBase):
+    pass
+
+
+# Properties shared by models stored in DB
+class OrganizationInDBBase(OrganizationBase):
+    id: int
+    title: str
+    description: str
+
+    class Config:
+        orm_mode = True
+
+
+# Properties to return to client
+class Organization(OrganizationInDBBase):
+    pass
+
+
+# Properties stored in DB
+class OrganizationInDB(OrganizationInDBBase):
+    pass
