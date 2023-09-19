7b125db78bb9e2961bb3670b1a4ee895570871a5
apetrosyan <apetrosyan@issart.com>
2023-09-19 20:32:34 +0600

    user-base-endpoints: update user scheme models

--- fastapi-test-project-apetrosyan/backend/app/app/schemas/user.py
+++ fastapi-test-project-apetrosyan/backend/app/app/schemas/user.py
@@ -8,18 +8,26 @@ class UserBase(BaseModel):
     email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
     is_superuser: bool = False
-    full_name: Optional[str] = None
+    first_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    last_name: Optional[str] = None
+    role: Optional[str] = None
+    description: Optional[str] = None
 
 
 # Properties to receive via API on creation
 class UserCreate(UserBase):
     email: EmailStr
     password: str
+    passwordConfirmation: str
+    first_name: str
+    last_name: str
+    role: str
 
 
 # Properties to receive via API on update
 class UserUpdate(UserBase):
-    password: Optional[str] = None
+    pass
 
 
 class UserInDBBase(UserBase):
