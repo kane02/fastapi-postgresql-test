e5abfa88eaf8b48cfa81b86a00abd6faa2782579
apetrosyan <apetrosyan@issart.com>
2023-09-19 20:32:17 +0600

    user-base-endpoints: update user model and related models

--- fastapi-test-project-apetrosyan/backend/app/app/models/user.py
+++ fastapi-test-project-apetrosyan/backend/app/app/models/user.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
 
 from app.db.base_class import Base
 
@@ -11,9 +10,15 @@ if TYPE_CHECKING:
 
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, index=True)
+    first_name = Column(String, index=False)
+    middle_name = Column(String, index=False)
+    last_name = Column(String, index=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-    items = relationship("Item", back_populates="owner")
+    role = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    created_at = Column(DateTime, nullable=True)
+    updated_at = Column(DateTime, nullable=True)
+    deleted_at = Column(DateTime, nullable=True)
